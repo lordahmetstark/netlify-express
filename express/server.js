8fb2f61e41b9e50f7fa6837bec57701134ea6d59
ahmetolgun <ahmet.olgun@gricreative.com>
2021-05-08 17:07:19 +0300

    node

--- express/server.js
+++ express/server.js
@@ -4,11 +4,21 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require("cors");
 const axios = require('axios');
 
 const router = express.Router();
 
+var corsOptions = {
+	origin: ["https://vigorous-visvesvaraya-983c08.netlify.app/home", "https://vigorous-visvesvaraya-983c08.netlify.app", "http://localhost:8080/", "http://localhost:8080"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 router.get('/', async (req, res) => {
+
+  console.log("biri geldi 11 >>");
 
   const instance = axios.create({
 		baseURL: 'https://developers.coinmarketcal.com/v1'
@@ -32,7 +42,22 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', async (req, res) => {
+  
+
+  console.log("biri geldi 22 >>");
+  const instance = axios.create({
+		baseURL: 'https://developers.coinmarketcal.com/v1'
+	});
+	
+	instance.defaults.headers.common['x-api-key'] = "uNqwYyxmgg1ffxdjNmBnm2RxFLk9iiTf7LDRkZZO";
+
+	let _response = await instance.get('/events?page=2&sortBy=created_desc&exchanges=binance&categories=8,11,14,4,1,17', {
+		timeout: 50000
+	});
+
+  return res.status(200).send(_response.data.body)
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
