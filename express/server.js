05946c5f318f311cda0a3d2805031b034dcc8578
ahmetolgun <ahmet.olgun@gricreative.com>
2021-05-08 16:28:57 +0300

    deneme

--- express/server.js
+++ express/server.js
@@ -4,12 +4,31 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+router.get('/', async (req, res) => {
+
+  const instance = axios.create({
+		baseURL: 'https://developers.coinmarketcal.com/v1'
+	});
+	
+	instance.defaults.headers.common['x-api-key'] = "uNqwYyxmgg1ffxdjNmBnm2RxFLk9iiTf7LDRkZZO";
+
+	let _response = await instance.get('/events?page=2&sortBy=created_desc&exchanges=binance&categories=8,11,14,4,1,17', {
+		timeout: 50000
+	});
+
+	console.log("_response.data.body >>", _response.data.body);
+
+  console.log("buradayım >>");
+
+  return res.status(200).send(_response.data.body)
+
+
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write('<h1>Hello from Express.js!</h1>');
+  // res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
