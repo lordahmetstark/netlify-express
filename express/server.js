f93bf818758c9822b13a0aa286e97aaf97b24b61
ahmetolgun <ahmet.olgun@gricreative.com>
2021-05-09 20:22:32 +0300

    mail

--- express/server.js
+++ express/server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const axios = require('axios');
 const Binance = require('node-binance-api');
+const nodemailer = require("nodemailer");
 
 const router = express.Router();
 
@@ -14,6 +15,49 @@ var corsOptions = {
 	origin: ["https://vigorous-visvesvaraya-983c08.netlify.app/home", "https://vigorous-visvesvaraya-983c08.netlify.app", "http://localhost:8080/", "http://localhost:8080"],
   credentials: true,
 };
+
+let sendMail = async (coin) => {
+
+  let smtp = {
+		user: "ahmet.olgun@faradayyazilim.com",
+		secure: true,
+		host: "smtp.yandex.com.tr",
+		port: 465,
+    auth: {
+      user: "ahmet.olgun@faradayyazilim.com",
+      pass: "1986Fm*-13-*"
+    }
+	}
+
+	let transporter = nodemailer.createTransport(smtp);
+
+  let _date = new Date();
+
+  
+	let mailTemplate = `<!doctype html> <html> <head> <meta name="viewport" content="width=device-width"> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> <title>Gulf Sigorta </title> <style> @media only screen and (max-width: 620px) { table[class=body] h1 { font-size: 28px !important; margin-bottom: 10px !important; } table[class=body] p, table[class=body] ul, table[class=body] ol, table[class=body] td, table[class=body] span, table[class=body] a { font-size: 16px !important; } table[class=body] .wrapper, table[class=body] .article { padding: 10px !important; } table[class=body] .content { padding: 0 !important; } table[class=body] .container { padding: 0 !important; width: 100% !important; } table[class=body] .main { border-left-width: 0 !important; border-radius: 0 !important; border-right-width: 0 !important; } table[class=body] .btn table { width: 100% !important; } table[class=body] .btn a { width: 100% !important; } table[class=body] .img-responsive { height: auto !important; max-width: 100% !important; width: auto !important; } } @media all { .ExternalClass { width: 100%; } .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div { line-height: 100%; } .apple-link a { color: inherit !important; font-family: inherit !important; font-size: inherit !important; font-weight: inherit !important; line-height: inherit !important; text-decoration: none !important; } #MessageViewBody a { color: inherit; text-decoration: none; font-size: inherit; font-family: inherit; font-weight: inherit; line-height: inherit; } .btn-primary table td:hover { background-color: #34495e !important; } .btn-primary a:hover { background-color: #34495e !important; border-color: #34495e !important; } } </style> </head> <body class="" style="background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;"> <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">This is preheader text. Some clients will show this text as a preview. </span> <table border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6;"> <tr> <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">&nbsp; </td> <td class="container" style="font-family: sans-serif; font-size: 14px; vertical-align: top; display: block; Margin: 0 auto; max-width: 580px; padding: 10px; width: 580px;"> <div class="content" style="box-sizing: border-box; display: block; Margin: 0 auto; max-width: 580px; padding: 10px;"> <!-- START CENTERED WHITE CONTAINER --> <table class="main" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background: #ffffff; border-radius: 3px;"> <!-- START MAIN CONTENT AREA --> <tr> <td class="wrapper" style="font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 20px;"> <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;"> <tr> <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;"> <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">${coin} ${_date} tarihinde satın alındı.. </p> </td> </tr> </table> </td> </tr> <!-- END MAIN CONTENT AREA --> </table> <!-- END CENTERED WHITE CONTAINER --> </div> </td> <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">&nbsp; </td> </tr> </table> </body> </html>`;
+
+  let mailOptions = {
+		from: "ahmet.olgun@faradayyazilim.com",
+		to: "ahmetolgun1986@gmail.com,emreakadal@gmail.com",
+		subject: `${coin} satın alındı`,
+		text: `${coin} satın alındı`,
+		html: mailTemplate
+	};
+
+  return new Promise((resolve, reject) => {
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				console.log('email error', error);
+				return reject(error);
+			}
+			console.log('Message %s sent: %s', info.messageId, info.response);
+			return resolve(info);
+		});
+	});
+
+
+
+}
 
 app.use(cors(corsOptions));
 
@@ -137,6 +181,10 @@ app.use('/bought-coins-for-emre', async (req, res) => {
   let tpPriceLimit = parseFloat((tpPrice * 0.9).toFixed(4))
   console.log("tpPrice >>", tpPrice);
   console.log("tpPriceLimit >>", tpPriceLimit);
+
+  sendMail(form.coin);
+  return res.status(200).send("OK")
+
 
 
   // let buyProccess = await binance.marketBuy(`${form.coin}${form.base}`, PositionSize);
